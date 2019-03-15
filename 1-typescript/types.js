3f31b120c735429b5c237fe10388979d57d3c516
Kumar Abhinav <abhinav0441@gmail.com>
2019-03-15 11:03:19 +0530

    zz

--- 1-typescript/types.js
+++ 1-typescript/types.js
@@ -1,5 +1,29 @@
 console.log("Hello World");
 var n = 3;
 var person;
-person = "Sam";
-person = 4;
+person = 'Sam';
+var c = 'a';
+var x;
+x = 23;
+x = 'David';
+var y = true;
+//y = 12; //Error
+//Arrays
+var numbers = [1, 2, 4, 6, -1, 1.2];
+console.log(numbers[0]);
+numbers.push(12);
+console.log(numbers);
+var lastElement = numbers.pop();
+console.log(numbers, lastElement);
+//Strings
+var city = "Banglore";
+var area = "Kadubheshanhalli";
+// let address =   city + ', ' + area;
+var address = " ----------\n" + city + ", " + area + "\n-----------";
+console.log(address);
+console.log(address.charAt(2));
+console.log(address.toUpperCase());
+//comment
+/*
+comment
+*/
--- 1-typescript/types.ts
+++ 1-typescript/types.ts
@@ -10,8 +10,36 @@ let x;
 x = 23;
 x = 'David';
 
+let y = true;
+//y = 12; //Error
 
+//Arrays
+let numbers : number[] = [1,2,4,6,-1, 1.2];
+console.log(numbers[0]);
 
+numbers.push(12);
+console.log(numbers);
+
+let lastElement = numbers.pop();
+console.log(numbers, lastElement);
+
+//Strings
+let city = "Banglore";
+let area = "Kadubheshanhalli";
+// let address =   city + ', ' + area;
+let address = ` ----------
+${city}, ${area}
+-----------`;
+console.log(address);
+console.log(address.charAt(2));
+console.log(address.toUpperCase());
+
+//boolean
+let isIndian = false;
+
+//null
+let val = null;
+let someVariable = undefined;
 
 //comment
 /*
