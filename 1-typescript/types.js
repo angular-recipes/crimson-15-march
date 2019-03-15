bff7528fd6b07bc67553f281b3fed6b30691c90f
Kumar Abhinav <abhinav0441@gmail.com>
2019-03-15 10:51:21 +0530

    zz

---
+++ 1-typescript/types.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+var n = 3;
+var person;
+person = "Sam";
+person = 4;
---
+++ 1-typescript/types.ts
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+let n : number = 3;
+let person : string;
+person = 'Sam';
+let c : string = 'a';
+
+let x;
+
+x = 23;
+x = 'David';
+
+
+
+
+//comment
+/*
+comment
+*/
+
+
+
+
+
