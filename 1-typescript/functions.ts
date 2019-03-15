bffb653804375dc8d10378fc4007530faee95777
Kumar Abhinav <abhinav0441@gmail.com>
2019-03-15 11:17:41 +0530

    zz

---
+++ 1-typescript/functions.ts
@@ -0,0 +1,23 @@
+// let add = function (n1, n2) {
+//     return n1 + n2;
+// }
+
+//fat arrow syntax
+// let add = (n1, n2) => {
+//     return n1 + n2;
+// }
+let add = (n1, n2) => n1 + n2;
+let square = n => n*n;
+
+console.log(square(3));
+console.log(add('Hello',6));
+
+let average = function(n1 : number, n2 : number, f: Function) {
+    return f(n1, n2)/2;
+};
+
+console.log(average(2,7, (n1,n2) => n1 + n2));
+
+
+
+
