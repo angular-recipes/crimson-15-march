95ee207c954fe5c183e8bca90e74fdeaf36c3aa2
Kumar Abhinav <abhinav0441@gmail.com>
2019-03-15 14:49:40 +0530

    zz

--- 1-typescript/classes.ts
+++ 1-typescript/classes.ts
@@ -1,12 +1,17 @@
-import { Human, hasSpeak } from './interfaces';
+import { Human, hasSpeak, hasAdd } from './interfaces';
 
-class Person implements hasSpeak{
+class Person implements hasSpeak, hasAdd{
     name: string;
     age: number;
+    n: number
 
     constructor(name, age) {
         this.name = name;
         this.age = age;
+    }
+
+    add(n1, n2) {
+        return n1+n2;
     }
 
     speak() {
@@ -23,9 +28,12 @@ sam.introduce();
 
 //Creating objects directly
 let david : Human = {
+    id: 1,
     name: 'David Boon',
     age: 21,
-    introduce: () => {
+    introduce: function() {
         console.log(`Hello I am ${this.name} and ${this.age} years old`);
     }
 };
+
+david.introduce();
--- 1-typescript/interfaces.ts
+++ 1-typescript/interfaces.ts
@@ -10,5 +10,10 @@ export interface hasSpeak {
     speak: Function;
 }
 
+export interface hasAdd {
+    n: number;
+    add(n1: number, n2: number) : number;
+}
+
 export let f = (c) => c*c;
 export const NUM_OF_USERS = 22;
